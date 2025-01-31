9bb2910de2150ca147ae35f64307214b50eb120a
Snowhazee <eaekeak@gmail.com>
2025-02-01 04:50:28 +0700

    Initial

---
+++ ServerProject/src/main/java/org/example/Client.java
@@ -0,0 +1,9 @@
+package org.example;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.net.Socket;
+public class Client {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+}
