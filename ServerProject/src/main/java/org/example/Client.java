41ccb9189d29423fc48540d6724bad702ee0eb74
Snowhazee <62649306+Snowhazee@users.noreply.github.com>
2025-02-01 04:55:54 +0700

    Delete ServerProject directory

--- ServerProject/src/main/java/org/example/Client.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.net.Socket;
-public class Client {
-    private Socket socket;
-    private BufferedReader bufferedReader;
-    private BufferedWriter bufferedWriter;
-}
